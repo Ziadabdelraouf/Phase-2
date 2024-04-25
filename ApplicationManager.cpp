7191b4804cf748664e8224d3a00a130b49475283
Ahmed16-gilgil <76847188+Ahmed16-gilgil@users.noreply.github.com>
2024-04-26 01:34:00 +0300

    Add files via upload

---
+++ ApplicationManager.cpp
@@ -0,0 +1,152 @@
+#include "ApplicationManager.h"
+#include "Actions\AddRectAction.h"
+
+#include "Actions\SelectAction.h"
+#include "Figures\CFigure.h"
+#include "Figures\CRectangle.h"
+#include "Actions\AddTriAction.h"
+#include "Actions\AddSquareAction.h"
+<<<<<<< HEAD
+=======
+#include "Actions\AddHexAction.h"
+#include "Actions\AddCrcAction.h"
+>>>>>>> Add figure is complete
+
+//Constructor
+ApplicationManager::ApplicationManager()
+{
+	//Create Input and output
+	pOut = new Output;
+	pIn = pOut->CreateInput();
+	
+	FigCount = 0;
+		
+	//Create an array of figure pointers and set them to NULL		
+	for(int i=0; i<MaxFigCount; i++)
+		FigList[i] = NULL;	
+}
+
+//==================================================================================//
+//								Actions Related Functions							//
+//==================================================================================//
+ActionType ApplicationManager::GetUserAction() const
+{
+	//Ask the input to get the action from the user
+	return pIn->GetUserAction();
+}
+////////////////////////////////////////////////////////////////////////////////////
+//Creates an action and executes it
+void ApplicationManager::ExecuteAction(ActionType ActType) 
+{
+	Action* pAct = NULL;
+	
+	//According to Action Type, create the corresponding action object
+	switch (ActType)
+	{
+		case DRAW_RECT:
+			pAct = new AddRectAction(this);
+			break;
+		case SELECT:
+			pAct = new SelectAction(this);
+			break;
+		case DRAW_SQR:
+			pAct = new AddSquareAction(this);
+			break;
+		case DRAW_TRI:
+			pAct = new AddTriAction(this);
+			break;
+<<<<<<< HEAD
+=======
+		case DRAW_HEX:
+			pAct = new AddHexAction(this);
+			break;
+		case DRAW_CIRCL:
+			pAct = new AddCrcAction(this);
+			break;
+
+		case CLEARALL:
+			pOut->ClearDrawArea();
+			break;
+
+>>>>>>> Add figure is complete
+		case EXIT:
+			///create ExitAction here
+			
+			break;
+		
+		case STATUS:	//a click on the status bar ==> no action
+			return;
+	}
+	
+	//Execute the created action
+	if(pAct != NULL)
+	{
+		pAct->Execute();//Execute
+		delete pAct;	//You may need to change this line depending to your implementation
+		pAct = NULL;
+	}
+}
+//==================================================================================//
+//						Figures Management Functions								//
+//==================================================================================//
+
+//Add a figure to the list of figures
+void ApplicationManager::AddFigure(CFigure* pFig)
+{
+	if(FigCount < MaxFigCount )
+		FigList[FigCount++] = pFig;	
+}
+////////////////////////////////////////////////////////////////////////////////////
+CFigure *ApplicationManager::GetFigure(int x, int y) const
+{
+	//If a figure is found return a pointer to it
+	for (int i = FigCount-1; i >=0 ; i--) {
+		if (FigList[i]->IsClickInside(x, y))
+			return FigList[i];
+	}
+	
+	//If this point (x,y) does not belong to any figure return NULL
+	return NULL;
+}
+
+CFigure* ApplicationManager::GetFigure(int id) const
+{
+	if (id <= FigCount)
+		return FigList[id]; //return pointer to figure if found
+	else
+		return NULL; //return NULL if figure not found
+}
+
+int ApplicationManager::GetFigureCount() const
+{
+	return FigCount; //returns actual number of figures
+}
+//==================================================================================//
+//							Interface Management Functions							//
+//==================================================================================//
+
+//Draw all figures on the user interface
+void ApplicationManager::UpdateInterface() const
+{	
+	for(int i=0; i<GetFigureCount(); i++)
+		FigList[i]->Draw(pOut);		//Call Draw function (virtual member fn)
+}
+////////////////////////////////////////////////////////////////////////////////////
+
+//Return a pointer to the input
+Input *ApplicationManager::GetInput() const
+{	return pIn; }
+
+//Return a pointer to the output
+Output *ApplicationManager::GetOutput() const
+{	return pOut; }
+
+////////////////////////////////////////////////////////////////////////////////////
+//Destructor
+ApplicationManager::~ApplicationManager()
+{
+	for(int i=0; i<FigCount; i++)
+		delete FigList[i];
+	delete pIn;
+	delete pOut;
+}
