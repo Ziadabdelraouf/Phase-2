e7ce36a78ad571e9b08210b8cd5e8485b2d8d1fb
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-07 22:03:38 +0300

    Update ShapeRNG.cpp

--- ShapeRNG.cpp
+++ ShapeRNG.cpp
@@ -5,19 +5,10 @@ void ShapeRNG::Execute() {
 	Input* pIn = pManager->GetInput();
 	
 	if (0==pManager->GetFigureCount()) {
-		pOut->ClearStatusBar();
 		pOut->PrintMessage("Error there is no shapes to start the play mode with");
 		return;
 	}
-	do {
-		srand(time(nullptr));
-		rshape = rand() % 5;
-	} while ((0 == rshape && 0 == R->getTriTotalCount()) ||
-		(1 == rshape && 0 == R->getSqrTotalCount()) ||
-		(2 == rshape && 0 == R->getRecTotalCount()) ||
-		(3 == rshape && 0 == R->getCircleTotalCount()) ||
-		(4 == rshape && 0 == R->getHexTotalCount()));
-	pOut->ClearStatusBar();
+	pManager->randNumGenBothS(rshape);
 	switch (rshape)
 	{
 	case triangle:
