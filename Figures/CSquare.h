cca23ee2b6d426d99e71be65930ecc5f68bd0f78
yousefwmorsy <166176410+yousefwmorsy@users.noreply.github.com>
2024-05-07 21:45:55 +0300

    Update CSquare.h

--- Figures/CSquare.h
+++ Figures/CSquare.h
@@ -8,9 +8,13 @@ private:
 	int sidelength;
 public:
 	CSquare(Point,GfxInfo FigureGfxInfo, int id);
+	CSquare(ifstream& fin, int ID);
+
 	virtual void Draw(Output* pOut) const;
 	virtual bool IsClickInside(int x, int y) const; //checks if click is inside the square
 	virtual void Save(ofstream& fout);
+	virtual void Load(ifstream& fin);
+
 	virtual void PrintInfo(Output* pOut) const; //prints info of square
 	virtual void SetSelected(bool s);
 	GfxInfo GetGfxInfo();
