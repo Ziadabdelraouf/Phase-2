3aea11f69cc79d5fb6b6b658970eb5f8fbe44331
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-07 22:02:54 +0300

    Update BothRNG.cpp

--- BothRNG.cpp
+++ BothRNG.cpp
@@ -5,24 +5,8 @@ void BothRNG::Execute() {
 	Input* pIn = pManager->GetInput();
 
 	if (0 != R->getFilledCount()) {
-			do {
-				srand(time(nullptr));
-				rcolor = rand() % 6;
-			} while ((rcolor == 0 && 0 == R->getBlackCount()) ||
-				(1 == rcolor && 0 == R->getYellowCount()) ||
-				(2 == rcolor && 0 == R->getOrangeCount()) ||
-				(3 == rcolor && 0 == R->getRedCount()) ||
-				(4 == rcolor && 0 == R->getGreenCount()) ||
-				(5 == rcolor && 0 == R->getBlueCount()));
-			do {
-				srand(time(nullptr));
-				rshape = rand() % 5;
-			} while ((0 == rshape && 0 == R->getTriTotalCount()) ||
-				(1 == rshape && 0 == R->getSqrTotalCount()) ||
-				(2 == rshape && 0 == R->getRecTotalCount()) ||
-				(3 == rshape && 0 == R->getCircleTotalCount()) ||
-				(4 == rshape && 0 == R->getHexTotalCount()));
-		
+		pManager->randNumGenBoth(rshape, rcolor);
+		num = pManager->getRandNumcount(rshape, rcolor);
 		switch (rshape)
 		{
 		case triangle:
