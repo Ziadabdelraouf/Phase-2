fb39bc066fc0861149ea66b676087a1f6e4b0149
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-07 22:02:12 +0300

    Update PlayBoth.cpp

--- PlayBoth.cpp
+++ PlayBoth.cpp
@@ -4,12 +4,11 @@ PlayBoth::PlayBoth(ApplicationManager* pApp) :BothRNG(pApp) {
 }
 void PlayBoth::Execute() {
 	BothRNG::Execute();
-	Output* pOut = pManager->GetOutput();
-	Input* pIn = pManager->GetInput();
 	if (num == 0) {
-		pOut->PrintMessage("number of the filled shapes with certain color equal zero try clicking again");
 		return;
 	}
+	Output* pOut = pManager->GetOutput();
+	Input* pIn = pManager->GetInput();
 	for (size_t i = 0; i < num; i++)  // hmark should have certain value but logic still has some problems
 	{
 		do {
@@ -22,7 +21,6 @@ void PlayBoth::Execute() {
 	}
 pOut->ClearStatusBar();
 pOut->PrintMessage("you scored " + to_string(score) + " out of " + to_string(num));
-
 }
 void PlayBoth::ReadActionParameters() {
 
