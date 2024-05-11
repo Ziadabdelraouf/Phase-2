12836258f0dd5faabd0b8fb85b8256b194d674c7
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-11 17:16:23 +0300

    Update PlayBoth.h

--- Actions/PlayBoth.h
+++ Actions/PlayBoth.h
@@ -3,8 +3,8 @@
 class PlayBoth:public BothRNG
 {private:
 	int score;  // player score
-	int x;
-	int y;
+	int x;      // it is the x of selected figure 
+	int y;      // it is the y of selected figure 
 public:
 	PlayBoth(ApplicationManager * pApp);
 	void Execute();
