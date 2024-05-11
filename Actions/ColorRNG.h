c419f0cb9df0e47764913be928debc120dcc69bf
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-11 17:06:50 +0300

    Update ColorRNG.h

--- Actions/ColorRNG.h
+++ Actions/ColorRNG.h
@@ -8,8 +8,8 @@
 class ColorRNG : public Action
 {
 protected:
-    int rcolor;
-    CFigure* R;
+    int rcolor; //the integer that express the color based on the defs.h (RNGcolor)
+    CFigure* R; //pointer used to access the functions of Cfigure
 public:
     void Execute();
     ColorRNG(ApplicationManager* pApp);
