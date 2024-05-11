6ea505328a55966608dda3add6bc0b192af82981
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-11 17:29:24 +0300

    Update PlayColor.cpp

--- Actions/PlayColor.cpp
+++ Actions/PlayColor.cpp
@@ -1,16 +1,16 @@
 #include "PlayColor.h"
 
 void PlayColor::Execute() {
-	ColorRNG::Execute();
+	ColorRNG::Execute(); //execute the code from colorRNG 
 	
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
-	if (R->getFilledCount()==0)
+	if (R->getFilledCount()==0)  //prevent the program from working if there is no filled shapes
 	{
 		return;
 	}
-	switch (rcolor)
-	{
+	switch (rcolor) //based on RNGcolor in defs.h
+	{ //the switch get the highest score (number of colored shapes with the wanted color)
 	case pBLACK:
 		hmark = R->getBlackCount();
 		break;
@@ -34,19 +34,19 @@ void PlayColor::Execute() {
 	for (int i = 0; i < hmark; i++)
 	{
 		do {
-			pIn->GetPointClicked(x, y);
+			pIn->GetPointClicked(x, y); //it get where the user clicked
 		} while (pManager->GetFigure(x, y) == NULL);
-		if (rcolor==pManager->GetFigure(x, y)->getFillClr())
+		if (rcolor==pManager->GetFigure(x, y)->getFillClr()) // condition to check if the selected figure is right
 		{
-			score++;
+			score++; //inc. the score if the condition is right
 		}
 		//implement delete here
 		if (pManager->GetFigure(x, y) != NULL) {
 			R = pManager->GetFigure(x, y);
-			pManager->usedBeforeDeleteInPlay(R);
+			pManager->usedBeforeDeleteInPlay(R); //used to make the selected figure the last one in figlist and dec the count of figures
 			delete R;
 			R == NULL;
-			pManager->UpdateInterface();
+			pManager->UpdateInterface(); //updates the interface to make the user see that the obj is deleted
 		}
 		//
 	}
