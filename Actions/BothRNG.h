a2c72e925851a2ece0f31417087dcad831efad1b
omar27105 <152551143+omar27105@users.noreply.github.com>
2024-05-11 17:05:52 +0300

    Update BothRNG.h

--- Actions/BothRNG.h
+++ Actions/BothRNG.h
@@ -7,12 +7,12 @@
 class BothRNG:public Action
 {
 protected:
-    int rcolor;
-    int rshape;
+    int rcolor; //the integer that express the color based on the defs.h (RNGcolor)
+    int rshape; //the integer that express the shape type based on the defs.h (RNGshape)
     int num;    //number of the figures to be clicked
-    CFigure* R;
-    string s1;
-    string s2;
+    CFigure* R; //pointer that is used to access cfigure functions
+    string s1; //string that is used at the first part of the text
+    string s2; //string that is used at the second part of the text (after the numbers of the shape to be picked)
 public:
     void Execute();
     BothRNG(ApplicationManager* pApp);
